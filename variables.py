7a3e16b0de9d6c1095c68a83b4e5b80baa5a3b53
Levi39922 <levi.prestidge@kingsway.school.nz>
2026-05-20 10:19:32 +1200

    My first program in Python

---
+++ variables.py
@@ -0,0 +1,36 @@
+greeting="hello world"
+print(greeting)
+
+say_hello ="hello"
+
+# Different
+string_eg1 = "this is a string."
+string_eg1 ="7"
+
+# Addition example
+num_1= "7"
+num_2= "5"
+
+sum=_1 + num_2
+print(sum)
+
+num_1=7
+num_2=5
+
+sum=num_1+ num_2
+
+
+# coffee program. chcking whether you like coffee
+like_coffee = input("Do you like coffee")
+print(li)
+
+# Demonstrate how to use an IF statement
+#Author: lEVI
+# 1 May 2026
+# Version 1
+
+# !TODO: Create a program that will ask the user whether
+# they like coffee or not. If they do not like coffee
+#I will try to persude 
+
+
